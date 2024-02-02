f53f7f62b03d045f9ac47bb2e80451426314081f
abhishekpaturkar <godkillop03@gmail.com>
2024-02-02 21:43:56 +0530

    Created Signup Route

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+dotenv.config();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World")
+// })
+
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server is running on PORT ${PORT}`)
+})
