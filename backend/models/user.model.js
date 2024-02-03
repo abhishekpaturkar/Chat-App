9d60a0c258853e8e74352e5a4fdd77f90456b1f8
abhishekpaturkar <godkillop03@gmail.com>
2024-02-03 13:49:03 +0530

    Message and Conversation model created

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    fullName : {
-        type : String,
-        required : true
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
     },
-    username : {
-        type : String,
-        required : true,
-        unique : true
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password : {
-        type : String,
-        minlength : 6,
-        required : true
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
     },
-    gender : {
-        type : String,
-        required : true,
-        enum : ['male', 'female']
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
     },
-    profilePic : {
-        type : String,
-        default : ""
-    }
-})
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
