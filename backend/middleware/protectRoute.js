6103d4b7978069b043624e99cce9c0c7c847349b
abhishekpaturkar <godkillop03@gmail.com>
2024-02-03 14:58:36 +0530

    Created Message Controller

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,7 @@ import User from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookie.jwt;
+    const token = req.cookies.jwt;
     if (!token) {
       return res
         .status(401)
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
-
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 
