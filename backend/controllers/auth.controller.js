3a056d7832687807c8494e85b2c51b33cf9716f2
abhishekpaturkar <godkillop03@gmail.com>
2024-02-03 13:05:29 +0530

    Logout Route Created

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,5 +79,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  console.log("logout");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
