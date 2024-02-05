836a2a139adb698ca04f938ec889fd21f4c45855
abhishekpaturkar <godkillop03@gmail.com>
2024-02-05 17:14:50 +0530

    added protected routes

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -43,6 +45,9 @@ const useSignup = () => {
       }
 
       // localstorage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      //context
+      setAuthUser(data);
     } catch (error) {
       toast.error(error.message);
     } finally {
