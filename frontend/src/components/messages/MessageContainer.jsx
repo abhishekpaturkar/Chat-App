acfaabb2e7a323bf0a249da6dee1c6bb7c31afcd
abhishekpaturkar <godkillop03@gmail.com>
2024-02-05 20:24:55 +0530

    added sidebar conversation functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,27 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
-    const noChatSelected = true;
+    const { selectedConversation, setSelectedConversation } = useConversation();
+
+    useEffect(() => {
+        // cleanup (unmount)
+        return () => setSelectedConversation(null);
+    }, [setSelectedConversation])
 
     return (
         <div className='md:min-w-[450px] flex flex-col'>
-            {noChatSelected ? <NoChatSelected /> : (
+            {!selectedConversation ? (<NoChatSelected />) : (
                 <>
                     {/* Header */}
                     <div className='bg-slate-500 px-4 py-2 mb-2'>
-                        <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John doe</span>
+                        <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
                     </div>
 
-                    <Messages />
+                    <Messages /> 
                     <MessageInput />
                 </>
             )}
@@ -28,10 +35,10 @@ const NoChatSelected = () => {
     return (
         <div className='flex items-center justify-center w-full h-full'>
             <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-                <p>Welcome 👋 John Doe ❄</p>
+                <p>Welcome 👋 Abhi Test ❄</p>
                 <p>Select a chat to start messaging</p>
                 <TiMessages className='text-3xl md:text-6xl text-center' />
             </div>
         </div>
     );
-};
+}; 
